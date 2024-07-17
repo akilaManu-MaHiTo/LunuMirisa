8e05e20788017dfb92bdd5f0da4b6cf313977161
SupunLiyanage88 <liyanagesupun10@gmail.com>
2024-07-17 19:23:38 +0530

    Revert "First UI"
    This reverts commit bce9df9eaf2829ee114317b078beb6cf6e30f8fe.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,12 @@
-// tailwind.config.js
-const {nextui} = require("@nextui-org/react");
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    // ...
-    // make sure it's pointing to the ROOT node_module
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [nextui()],
-};
+  plugins: [],
+}
+
