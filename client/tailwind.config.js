bce9df9eaf2829ee114317b078beb6cf6e30f8fe
SupunLiyanage88 <liyanagesupun10@gmail.com>
2024-07-17 15:05:45 +0530

    First UI

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,16 @@
+// tailwind.config.js
+const {nextui} = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  darkMode: "class",
+  plugins: [nextui()],
+};
