feae15e28b5a37bb2579a8e56bf822e64fd0d458
SupunLiyanage88 <liyanagesupun10@gmail.com>
2024-07-25 10:19:46 +0530

    homepage and footer

--- client/src/views/UserHome.jsx
+++ client/src/views/UserHome.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import logo from '../Images/Logo.png'; 
-import homePic from '../Images/food.jpg';
+import homePic from '../Images/food.png';
 import NavigationBar from './NavigationBar'; 
 import Footer from './Footer.jsx'; 
 import food from '../Images/food.svg';
@@ -16,7 +16,7 @@ const UserHome = () => {
       <NavigationBar logo={logo} /> 
 
       <div className="flex  w-full h-full">
-        <img src={homePic} alt="Home" className="w-full h-auto rounded opacity-50" style={{ filter: 'blur(1px)' }} /> 
+        <img src={homePic} alt="Home" className="w-full h-auto rounded" style={{ filter: 'blur(1px)' }} /> 
         <div className='absolute text-white w-[50rem] pl-[10rem] hidden md:inline text-7xl pt-32 font-spartan'>Experience the Authentic Sri Lankan Foods</div>
         <div className='absolute text-white hidden custom-md:flex justify-center text-2xl w-full mt-[50rem]'>
           <div className='font-spartan font-thin bg-custom-black p-4 opacity-70 '> Order Now ↓ </div>
@@ -30,11 +30,11 @@ const UserHome = () => {
       <div className='flex justify-center gap-y-8'>
         <div className="flex flex-wrap gap-x-16 gap-y-8 pt-16 pb-10 justify-center w-[80rem]">
           {[...Array(3)].map((_, index) => (
-            <div key={index} className="bg-custom-gray h-[34rem] p-6 rounded shadow-md w-[22rem] max-w-md mb-4 md:mb-0 flex flex-col items-center transition-transform duration-500 ease-in-out transform hover:scale-105">
-              <img src={food} alt="food" className='w-[19rem] h-auto rounded mx-auto mt-2' />
-              <div className="text-center text-white font-spartan font-thin mt-8 text-3xl">Chicken Biriyani</div>
-              <div className="text-center text-white font-spartan font-thin text-2xl mt-2">Rs.1700/-</div>
-              <button type="submit" className='flex items-center justify-center w-[17rem] py-1 mt-16 bg-custom-light text-white hover:bg-custom-black h-12 transition-all duration-300 ease-in-out transform hover:scale-105'>
+            <div key={index} className="bg-custom-gray p-6 rounded shadow-md w-[18rem] h-auto max-w-md mb-4 md:mb-0 flex flex-col items-center transition-all duration-500 ease-in-out transform  ">
+            <img src={food} alt="food" className='w-[15rem] h-auto rounded mx-auto mt-2' />
+              <div className="text-center text-white font-spartan font-thin mt-8 text-2xl">Chicken Biriyani</div>
+              <div className="text-center text-white font-spartan font-thin text-xl mt-2">Rs.1700/-</div>
+              <button type="submit" className='flex mb-4 items-center justify-center w-[15rem] py-1 mt-16 bg-custom-light text-white hover:bg-custom-black h-12 transition-all duration-300 ease-in-out transform hover:scale-105'>
               <FontAwesomeIcon icon={faCartPlus} className='mr-2' /> Add to Cart </button>
             </div>
           ))}
@@ -42,16 +42,16 @@ const UserHome = () => {
       </div>
     
 
-      <div className='text-white text-5xl pt-20 font-spartan font-thin pl-[10rem]'>Popular Meals</div>
+      <div className='text-white text-5xl pt-8 font-spartan font-thin pl-[10rem]'>Popular Meals</div>
 
       <div className='flex justify-center gap-y-8'>
         <div className="flex flex-wrap gap-x-16 gap-y-8 pt-16 pb-10 justify-center w-[80rem]">
           {[...Array(3)].map((_, index) => (
-            <div key={index} className="bg-custom-gray h-[34rem] p-6 rounded shadow-md w-[22rem] max-w-md mb-4 md:mb-0 flex flex-col items-center transition-transform duration-500 ease-in-out transform hover:scale-105">
-              <img src={food} alt="food" className='w-[19rem] h-auto rounded mx-auto mt-2' />
-              <div className="text-center text-white font-spartan font-thin mt-8 text-3xl">Chicken Biriyani</div>
-              <div className="text-center text-white font-spartan font-thin text-2xl mt-2">Rs.1700/-</div>
-              <button type="submit" className='flex items-center justify-center w-[17rem] py-1 mt-16 bg-custom-light text-white hover:bg-custom-black h-12 transition-all duration-300 ease-in-out transform hover:scale-105'>
+            <div key={index} className="bg-custom-gray p-6 rounded shadow-md w-[18rem] h-auto max-w-md mb-4 md:mb-0 flex flex-col items-center transition-transform duration-500 ease-in-out transform ">
+              <img src={food} alt="food" className='w-[15rem] h-auto rounded mx-auto mt-2' />
+              <div className="text-center text-white font-spartan font-thin mt-8 text-2xl">Chicken Biriyani</div>
+              <div className="text-center text-white font-spartan font-thin text-xl mt-2">Rs.1700/-</div>
+              <button type="submit" className='flex mb-4 items-center justify-center w-[15rem] py-1 mt-16 bg-custom-light text-white hover:bg-custom-black h-12 transition-all duration-300 ease-in-out transform hover:scale-105'>
               <FontAwesomeIcon icon={faCartPlus} className='mr-2' /> Add to Cart </button>
             </div>
           ))}
