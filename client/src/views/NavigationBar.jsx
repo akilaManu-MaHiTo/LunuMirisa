3b35a78f2eab78d0a82cc44c33a466d6e5607a75
SupunLiyanage88 <liyanagesupun10@gmail.com>
2024-08-28 17:21:22 +0530

    navi bar 1.0

--- client/src/views/NavigationBar.jsx
+++ client/src/views/NavigationBar.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faUserCog } from '@fortawesome/free-solid-svg-icons';
 import { useParams } from 'react-router-dom';
 
 const NavigationBar = ({ logo }) => {
@@ -57,37 +59,62 @@ const NavigationBar = ({ logo }) => {
           <li><Link to="/" className="text-white font-spartan font-thin text-2xl">Offers</Link></li>
         </ul>
         <div className="flex items-center space-x-4">
-          <FontAwesomeIcon icon={faSearch} className="text-white cursor-pointer hidden md:inline text-2xl p-3" onClick={toggleSearchBar} />
-          <FontAwesomeIcon icon={faShoppingCart} className="text-white cursor-pointer hidden md:inline text-2xl p-3" />
-          <FontAwesomeIcon icon={faUser} className="text-white cursor-pointer text-2xl p-3" onClick={toggleUserOption} />
+                      <FontAwesomeIcon 
+                icon={faSearch} 
+                className="text-white cursor-pointer hidden md:inline text-2xl p-3 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:text-gray-300"
+                onClick={toggleSearchBar} 
+              />
+
+              <FontAwesomeIcon 
+                icon={faShoppingCart} 
+                className="text-white cursor-pointer hidden md:inline text-2xl p-3 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:text-gray-300"
+              />
+
+              <FontAwesomeIcon 
+                icon={faUser} 
+                className="text-white cursor-pointer text-2xl p-3 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:text-gray-300"
+                onClick={toggleUserOption} 
+              />
+
         </div>
         {showSearchBar && (
-          <div ref={searchRef} className="absolute top-0.5 right-60 mt-2 mr-4 p-2 rounded-md shadow-lg hidden md:inline h-15.5" style={{ maxWidth: '600px' }}>
-            <input type="text" placeholder="Search..." className=" text-white px-3 py-1 border border-black focus:outline-none focus:border-black w-full md:max-w-96 bg-gray-700" />
+          <div
+            ref={searchRef}
+            className="absolute top-0.5 right-60 mt-2 mr-4 p-2 rounded-md shadow-lg hidden md:inline h-15.5 transform transition-transform duration-300 ease-in-out scale-95 translate-y-[-10px]"
+            style={{ maxWidth: '600px' }}
+          >
+            <input
+              type="text"
+              placeholder="Search..."
+              className="text-white px-3 py-1 border border-black focus:outline-none focus:border-black w-full md:max-w-96 bg-gray-700"
+            />
           </div>
-        )}
+            )}
 
-{showUserOption && (
-  <div 
-    ref={userRef} 
-    className="absolute top-10 right-16 mt-2 p-4 rounded-lg bg-gray-800 shadow-lg border border-gray-700"
-  >
-    <ul className="space-y-2">
-      <li>
-        <Link to={`/UserProfile/${userId}`}>
-          <button className="w-full text-white text-left font-semibold hover:bg-gray-700 py-2 px-3 rounded-md transition-colors duration-300">
-            Profile Settings
-          </button>
-        </Link>
-      </li>
-      <li>
-        <button className="w-full text-red-500 font-semibold hover:text-red-600 py-2 px-3 rounded-md transition-colors duration-300">
-          Logout
-        </button>
-      </li>
-    </ul>
-  </div>
-)}
+            {showUserOption && (
+              <div 
+                ref={userRef} 
+                className="absolute top-10 right-20 mt-14 p-4 rounded-lg bg-custom-dark shadow-lg border border-white transform transition-transform duration-300 ease-in-out scale-95 translate-y-[-10px]"
+              >
+                <ul className="space-y-2">
+                  <li>
+                    <Link to={`/UserProfile/${userId}`}>
+                      <button className="w-full text-white text-left font-semibold hover:bg-white hover:text-black py-2 px-3 rounded-md transition-colors duration-300 flex items-center">
+                        <FontAwesomeIcon icon={faUserCog} className="mr-2" />
+                        Profile Settings
+                      </button>
+                    </Link>
+                  </li>
+                  <li>
+                    <button className="w-full text-red-500 font-semibold hover:bg-red-500 hover:text-black py-2 px-3 rounded-md transition-colors duration-300 flex items-center">
+                      <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+                      Logout
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
+
 
       </nav>
     </div>
