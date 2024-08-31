e1db9aeda37d44767c029c4e8c49b52ecbad5c3c
SupunLiyanage88 <liyanagesupun10@gmail.com>
2024-08-31 15:30:19 +0530

    User profile 1.0(Supun)

---
+++ client/src/views/Components/ProfilePic.jsx
@@ -0,0 +1,60 @@
+// ProfilePic.jsx
+import React, { useEffect } from 'react';
+import './Profile.css';
+import $ from 'jquery';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+
+
+
+function ProfilePic() {
+    useEffect(() => {
+        const readURL = (input) => {
+            if (input.files && input.files[0]) {
+                const reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+                    $('#imagePreview').hide();
+                    $('#imagePreview').fadeIn(650);
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        };
+
+        $("#imageUpload").change(function () {
+            readURL(this);
+        });
+    }, []); 
+
+    return (
+        <div className='flex items-center justify-center w-30'>
+        <div className="relative w-40 h-40">
+            <div 
+                id="imagePreview" 
+                className="w-full h-full bg-cover bg-center rounded-full" 
+                style={{ backgroundImage: 'url(http://i.pravatar.cc/500?img=7)' }}
+            ></div>
+            <div className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2 flex items-center justify-center">
+                <input 
+                    type="file" 
+                    id="imageUpload" 
+                    accept=".png, .jpg, .jpeg" 
+                    className="hidden" 
+                />
+                <label 
+                    htmlFor="imageUpload" 
+                    className="flex items-center justify-center"
+                >
+                    <FontAwesomeIcon 
+                        icon={faPencilAlt} 
+                        className="text-black text-[1rem] bg-gray-600 p-2 rounded-full hover:bg-white mb-10" 
+                    />
+                </label>
+            </div>
+        </div>
+    </div>
+    
+    );
+}
+
+export default ProfilePic;
