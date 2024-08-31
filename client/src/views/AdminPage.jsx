fcbdbb559e5b73269b85dce07df02aaf61a83509
AkilaManu <akilamanujith@gmail.com>
2024-08-31 16:01:54 +0530

    admin navi Bar(akila)

--- client/src/views/AdminPage.jsx
+++ client/src/views/AdminPage.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AdminNaviBar from './Components/AdminNavigationBar'
 
 const AdminPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded shadow-md w-full max-w-md">
-        <h1 className="text-3xl font-bold mb-4 text-center">Welcome to Admin</h1>
-        <p className="text-center">This is the user Admin page.</p>
+    <div>
+      <AdminNaviBar/>
+      
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white p-6 rounded shadow-md w-full max-w-md">
+          <h1 className="text-3xl font-bold mb-4 text-center">Welcome to Admin</h1>
+          <p className="text-center">This is the user Admin page.</p>
+        </div>
+
+        <Link to="/AddMenuList"><button>Menu List</button></Link>
+        <Link to="/ShowInventory"><button>Inventory</button></Link>
+        <Link to="/AddTables"><button>AddTables</button></Link>
+        <Link to="/ShowSupplierOrder"><button>Supplier Orders</button></Link>
+
+
+
+
+
       </div>
-
-      <Link to="/AddMenuList"><button>Menu List</button></Link>
-      <Link to="/ShowInventory"><button>Inventory</button></Link>
-      <Link to="/AddTables"><button>AddTables</button></Link>
-      <Link to="/ShowSupplierOrder"><button>Supplier Orders</button></Link>
-
-
-
-
-
     </div>
   );
 };
---
+++ client/src/views/Components/AdminNavigationBar.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useRef } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faShoppingCart, faUser, faSignOutAlt, faUserCog } from '@fortawesome/free-solid-svg-icons';
+import logo from '../../Images/Logo.png';
+
+const AdminNavigationBar = () => {
+  const [showUserOption, setShowUserOption] = useState(false);
+
+  const toggleUserOption = () => setShowUserOption(!showUserOption);
+
+  return (
+    <div className='custom1-md:pr-[10rem] custom1-md:pl-[10rem] bg-custom-gray'>
+      <nav className="flex items-center justify-between px-4">
+        <div className="flex items-center h-[7rem] w-32">
+          <img src={logo} alt="Logo" className="h-auto w-20 " />
+        </div>
+        
+        <div className="flex items-center space-x-4">
+  <h4 className="text-white font-semibold text-lg tracking-wide">Login / Signup</h4>
+
+  <FontAwesomeIcon 
+    icon={faUser} 
+    className="text-white cursor-pointer text-2xl p-3 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:text-gray-300"
+    onClick={toggleUserOption} 
+  />
+</div>
+
+      </nav>
+    </div>
+  );
+};
+
+export default AdminNavigationBar;
