4bde8c3b1a2863652810aad0b3a70362d9189be7
AkilaManu <akilamanujith@gmail.com>
2024-09-18 18:24:53 +0530

    orders of inventory

--- server/models/InventoryOrder.js
+++ server/models/InventoryOrder.js
@@ -1,14 +1,32 @@
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose');
 
 const InventoryOrderSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true, // Name is required
+  },
+  image: {
+    type: String,
+  },
+  quantity: {
+    type: Number,
+    required: true, // Quantity is required
+  },
+  maxQuantity: {
+    type: Number,
+    required: true, // Max Quantity is required
+  },
+  category: {
+    type: String,
+    required: true, // Category is required
+    enum: ['Vegetables', 'Fruits', 'Spices', 'Meat', 'Fisheries'], // Valid categories
+  },
+  orderQuantity: {
+    type: Number,
+    required: true, // Order quantity (derived from maxQuantity - quantity)
+  },
+});
 
-    name: String,
-    image: String,
-    quantity: String,
-    maxQuantity: String,
-    category: String,
-    
-})
 
-const InventoryOrderModel = mongoose.model("inventoryOrders",InventoryOrderSchema)
-module.exports = InventoryOrderModel
+const InventoryOrderModel = mongoose.model('InventoryOrder', InventoryOrderSchema);
+module.exports = InventoryOrderModel;
