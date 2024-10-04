7e949bf281fb1d486ba9124e9a3a5b47a6e2c4c6
SupunLiyanage88 <liyanagesupun10@gmail.com>
2024-10-04 11:14:09 +0530

    fix2

--- client/src/views/UserHome.jsx
+++ client/src/views/UserHome.jsx
@@ -188,63 +188,133 @@ const UserHome = () => {
           </div>
         </div>
 
-
-
-
-
-
-
-
-  <div>
-    <div className="text-white text-5xl mt-16 mb-16 font-spartan font-thin pl-[10rem]">
-      What keeps you coming back to us?
-    </div>
-    {reviews.length > 0 && reviews[currentReviewIndex] ? (
-      <div
-        className={`w-full px-10 transition-all duration-700 ease-in-out transform ${
-          fade ? 'opacity-100 translate-x-0' : 'opacity-0'
-        }`}
-      >
-        <div
-          key={currentReviewIndex}
-          className="p-10 mb-20 rounded my-4 w-[65rem] h-[25rem] mx-auto"
-          style={{
-            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${bgReview})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        >
-          <div className="flex flex-col items-start">
-            <div className="flex items-start">
-              <img
-                src={`http://localhost:3001/Images/${reviews[currentReviewIndex].profileImage}`}
-                alt={`${reviews[currentReviewIndex].FirstName} ${reviews[currentReviewIndex].LastName}`}
-                className="w-16 h-16 rounded-full mr-4"
-              />
-              <div className="flex flex-col">
-                <h3 className="text-xl font-bold text-white">
-                  {reviews[currentReviewIndex].FirstName} {reviews[currentReviewIndex].LastName}
-                </h3>
-                <div className="flex flex-col mt-2">
-                  <div className="flex items-center">{renderStars(reviews[currentReviewIndex].rating)}</div>
+        <div className="text-white text-5xl mt-16 mb-16 font-spartan font-thin pl-[10rem]">
+          What keeps you coming back to us?
+        </div>
+        {reviews.length > 0 && reviews[currentReviewIndex] ? (
+          <div
+            className={`w-full px-10 transition-all duration-700 ease-in-out transform ${
+              fade ? 'opacity-100 translate-x-0' : 'opacity-0'
+            }`}
+          >
+            <div
+              key={currentReviewIndex}
+              className="p-10 mb-20 rounded my-4 w-[65rem] h-[25rem] mx-auto"
+              style={{
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${bgReview})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            >
+              <div className="flex flex-col items-start">
+                <div className="flex items-start">
+                  <img
+                    src={`http://localhost:3001/Images/${reviews[currentReviewIndex].profileImage}`}
+                    alt={`${reviews[currentReviewIndex].FirstName} ${reviews[currentReviewIndex].LastName}`}
+                    className="w-16 h-16 rounded-full mr-4"
+                  />
+                  <div className="flex flex-col">
+                    <h3 className="text-xl font-bold text-white">
+                      {reviews[currentReviewIndex].FirstName} {reviews[currentReviewIndex].LastName}
+                    </h3>
+                    <div className="flex flex-col mt-2">
+                      <div className="flex items-center">{renderStars(reviews[currentReviewIndex].rating)}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <p className="text-gray-300 text-2xl font-bold italic mt-5">
+                  {reviews[currentReviewIndex].reviewTitle}
+                </p>
+                <div className="bg-white bg-opacity-70 h-[11rem] pl-4 pr-4 pb-4 pt-1 mt-3 rounded-xl">
+                  <p className="text-black italic mt-5">{reviews[currentReviewIndex].review}</p>
                 </div>
               </div>
             </div>
           </div>
-          <div>
-            <p className="text-gray-300 text-2xl font-bold italic mt-5">
-              {reviews[currentReviewIndex].reviewTitle}
-            </p>
-            <div className="bg-white bg-opacity-70 h-[11rem] pl-4 pr-4 pb-4 pt-1 mt-3 rounded-xl">
-              <p className="text-black italic mt-5">{reviews[currentReviewIndex].review}</p>
+        ) : (
+          <div
+            class="flex flex-col gap-2 dark:text-white max-w-md w-full bg-white dark:bg-neutral-900 p-5 rounded-md mt-8 shadow-md hover:scale-105 hover:duration-150 duration-150"
+          >
+            <div class="flex flex-row justify-between w-full">
+              <div class="flex flex-row justify-between w-full">
+                <div
+                  class="bg-gray-200 dark:bg-neutral-700 rounded-md w-20 h-4 animate-pulse"
+                ></div>
+                <div
+                  class="bg-gray-200 dark:bg-neutral-700 rounded-md w-10 animate-pulse"
+                ></div>
+              </div>
             </div>
+            <div class="flex flex-row justify-between w-full">
+              <div
+                class="bg-gray-200 dark:bg-neutral-700 rounded-md w-40 animate-pulse"
+              ></div>
+
+              <div class="text-xs">
+                <div class="flex flex-row">
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    class="h-4 w-4 text-yellow-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9.049 2.927c.3-.916 1.603-.916 1.902 0l1.286 3.953a1.5 1.5 0 001.421 1.033h4.171c.949 0 1.341 1.154.577 1.715l-3.38 2.458a1.5 1.5 0 00-.54 1.659l1.286 3.953c.3.916-.757 1.67-1.539 1.145l-3.38-2.458a1.5 1.5 0 00-1.76 0l-3.38 2.458c-.782.525-1.838-.229-1.539-1.145l1.286-3.953a1.5 1.5 0 00-.54-1.659l-3.38-2.458c-.764-.561-.372-1.715.577-1.715h4.171a1.5 1.5 0 001.421-1.033l1.286-3.953z"
+                    ></path>
+                  </svg>
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    class="h-4 w-4 text-yellow-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9.049 2.927c.3-.916 1.603-.916 1.902 0l1.286 3.953a1.5 1.5 0 001.421 1.033h4.171c.949 0 1.341 1.154.577 1.715l-3.38 2.458a1.5 1.5 0 00-.54 1.659l1.286 3.953c.3.916-.757 1.67-1.539 1.145l-3.38-2.458a1.5 1.5 0 00-1.76 0l-3.38 2.458c-.782.525-1.838-.229-1.539-1.145l1.286-3.953a1.5 1.5 0 00-.54-1.659l-3.38-2.458c-.764-.561-.372-1.715.577-1.715h4.171a1.5 1.5 0 001.421-1.033l1.286-3.953z"
+                    ></path>
+                  </svg>
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    class="h-4 w-4 text-yellow-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9.049 2.927c.3-.916 1.603-.916 1.902 0l1.286 3.953a1.5 1.5 0 001.421 1.033h4.171c.949 0 1.341 1.154.577 1.715l-3.38 2.458a1.5 1.5 0 00-.54 1.659l1.286 3.953c.3.916-.757 1.67-1.539 1.145l-3.38-2.458a1.5 1.5 0 00-1.76 0l-3.38 2.458c-.782.525-1.838-.229-1.539-1.145l1.286-3.953a1.5 1.5 0 00-.54-1.659l-3.38-2.458c-.764-.561-.372-1.715.577-1.715h4.171a1.5 1.5 0 001.421-1.033l1.286-3.953z"
+                    ></path>
+                  </svg>
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    class="h-4 w-4 text-yellow-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9.049 2.927c.3-.916 1.603-.916 1.902 0l1.286 3.953a1.5 1.5 0 001.421 1.033h4.171c.949 0 1.341 1.154.577 1.715l-3.38 2.458a1.5 1.5 0 00-.54 1.659l1.286 3.953c.3.916-.757 1.67-1.539 1.145l-3.38-2.458a1.5 1.5 0 00-1.76 0l-3.38 2.458c-.782.525-1.838-.229-1.539-1.145l1.286-3.953a1.5 1.5 0 00-.54-1.659l-3.38-2.458c-.764-.561-.372-1.715.577-1.715h4.171a1.5 1.5 0 001.421-1.033l1.286-3.953z"
+                    ></path>
+                  </svg>
+
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    class="h-4 w-4 text-yellow-200"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9.049 2.927c.3-.916 1.603-.916 1.902 0l1.286 3.953a1.5 1.5 0 001.421 1.033h4.171c.949 0 1.341 1.154.577 1.715l-3.38 2.458a1.5 1.5 0 00-.54 1.659l1.286 3.953c.3.916-.757 1.67-1.539 1.145l-3.38-2.458a1.5 1.5 0 00-1.76 0l-3.38 2.458c-.782.525-1.838-.229-1.539-1.145l1.286-3.953a1.5 1.5 0 00-.54-1.659l-3.38-2.458c-.764-.561-.372-1.715.577-1.715h4.171a1.5 1.5 0 001.421-1.033l1.286-3.953z"
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+            </div>
+
+            <div
+              class="bg-gray-200 dark:bg-neutral-700 rounded-md w-full h-20 animate-pulse"
+            ></div>
           </div>
-        </div>
-      </div>
-    ) : (
-      <div className="text-white text-center">No reviews available</div>
-    )}
-  </div>
+
+        )}
 
       </section>
 
