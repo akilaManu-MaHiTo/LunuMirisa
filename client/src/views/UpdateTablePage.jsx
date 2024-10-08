d41434e62532e18136c6974abedc77915534396c
SupunLiyanage88 <liyanagesupun10@gmail.com>
2024-10-08 13:52:45 +0530

    fix

--- client/src/views/UpdateTablePage.jsx
+++ client/src/views/UpdateTablePage.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
+import AdminNaviBar from './Components/AdminNavigationBar';
+import Sidebar from './Components/ToggleSlideBar';
+import bgtable from '../Images/suppliar-bg.jpg';
 
 function UpdateTable() {
   const { id } = useParams();
@@ -30,55 +33,66 @@ function UpdateTable() {
   };
 
   return (
-    <div className="max-w-md mx-auto bg-white p-8 border border-gray-300 rounded-lg shadow-md">
-      <Link 
-        to="/ShowTables" 
-        className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-      >
-        My Tables
-      </Link>
-      <h2 className="text-2xl font-bold mb-6 text-center">Update Table Form</h2>
-      {error && <p className="text-red-500 text-center mb-4">{error}</p>}
-      <form onSubmit={updateTable}>
-        <div className="mb-4">
-          <label className="block text-gray-700">Table Number:</label>
-          <input
-            type="number"
-            className="w-full p-2 border border-gray-300 rounded mt-1"
-            required
-            value={tableNum}
-            onChange={(e) => setTableNum(e.target.value)}
-            readOnly
-          />
+    <div>
+      <AdminNaviBar selectedPage="Add Table Details" />
+      <Sidebar />  
+        <div className="max-w-md mx-auto bg-white p-8 border mt-20 mb-20 border-gray-300 rounded-lg shadow-md">
+
+          <h2 className="text-2xl font-bold my-6 text-center">Update Table Form</h2>
+
+          <div className='ml-64 my-10'>
+          <Link 
+            to="/ShowTables" 
+            className="bg-black hover:bg-white justify-center hover:border hover:border-black hover:text-black transition-all ease-out scale-105 duration-500 text-white font-bold py-2 px-4 rounded"
+          >
+            My Tables
+          </Link>
+          </div>
+
+  
+
+          {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+          <form onSubmit={updateTable}>
+            <div className="mb-4">
+              <label className="block text-gray-700">Table Number:</label>
+              <input
+                type="number"
+                className="w-full p-2 border border-gray-300 rounded mt-1"
+                required
+                value={tableNum}
+                onChange={(e) => setTableNum(e.target.value)}
+                readOnly
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Quantity:</label>
+              <input
+                type="number"
+                className="w-full p-2 border border-gray-300 rounded mt-1"
+                required
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Price (Rs):</label>
+              <input
+                type="number"
+                className="w-full p-2 border border-gray-300 rounded mt-1"
+                required
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-black text-white p-2 rounded mt-4 hover:bg-white hover:border hover:border-black hover:text-black transition-all ease-out scale-105 duration-500"
+            >
+              Update Table
+            </button>
+          </form>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Quantity:</label>
-          <input
-            type="number"
-            className="w-full p-2 border border-gray-300 rounded mt-1"
-            required
-            value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
-            
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Price (Rs):</label>
-          <input
-            type="number"
-            className="w-full p-2 border border-gray-300 rounded mt-1"
-            required
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-green-500 text-white p-2 rounded mt-4 hover:bg-green-600"
-        >
-          Update Table
-        </button>
-      </form>
     </div>
   );
 }
