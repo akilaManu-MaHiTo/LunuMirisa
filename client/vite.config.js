d18f89db7f88f1eea10f04429debbc9e3f1cf038
AkilaManu <akilamanujith@gmail.com>
2024-12-14 18:50:58 +0530

    host make 1

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,16 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-
   build: {
     outDir: 'dist', // Specifies the output directory for the build
   },
-
   resolve: {
     alias: {
       '@': '/src', // Alias for cleaner imports
     },
   },
-
-  base: './', // Ensures correct asset paths for production builds
+  base: '/', // Ensures correct asset paths in production
 });
